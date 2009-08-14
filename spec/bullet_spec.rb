2637d0597d70dd493e6cfb2d27e0bb4d5e5a4cc3
Richard <richard@ekohe.com>
2009-08-14 18:03:55 +0800

    test array associations, :category => [:posts, :entries]

--- spec/bullet_spec.rb
+++ spec/bullet_spec.rb
@@ -16,6 +16,11 @@ describe Bullet do
         t.column :name, :string
         t.column :post_id, :integer
       end
+
+      create_table :entries do |t|
+        t.column :name, :string
+        t.column :category_id, :integer
+      end
     end
   end
 
@@ -34,6 +39,10 @@ describe Bullet do
     belongs_to :category
     has_many :comments
   end
+  
+  class Entry < ActiveRecord::Base
+    belongs_to :category
+  end
 
   class Comment < ActiveRecord::Base
     belongs_to :post
@@ -42,13 +51,18 @@ describe Bullet do
   before(:all) do
     setup_db
     
-    category = Category.create!(:name => 'first')
-    post = category.posts.create!(:name => 'first')
-    post.comments.create!(:name => 'first')
-    post.comments.create!(:name => 'second')
-    post = category.posts.create!(:name => 'second')
-    post.comments.create!(:name => 'third')
-    post.comments.create!(:name => 'fourth')
+    category = Category.create(:name => 'first')
+    
+    post = category.posts.create(:name => 'first')
+    post = category.posts.create(:name => 'second')
+    
+    post.comments.create(:name => 'first')
+    post.comments.create(:name => 'second')
+    post.comments.create(:name => 'third')
+    post.comments.create(:name => 'fourth')
+    
+    entry = category.entries.create(:name => 'first')
+    entry = category.entries.create(:name => 'second')
   end
   
   after(:all) do
@@ -104,6 +118,38 @@ describe Bullet do
         category.posts.each do |post|
           post.comments.collect(&:name)
         end
+      end
+      Bullet::Association.unpreload_associations.should_not be_empty
+      Bullet::Association.end_request
+    end
+  end
+  
+  context "category => posts, category => entries" do
+    it "should detect preload with category => [posts, entries]" do
+      Bullet::Association.start_request
+      Category.find(:all, :include => [:posts, :entries]).each do |category|
+        category.posts.collect(&:name)
+        category.entries.collect(&:name)
+      end
+      Bullet::Association.unpreload_associations.should be_empty
+      Bullet::Association.end_request
+    end
+
+    it "should detect preload with category => posts, but no category => entries" do
+      Bullet::Association.start_request
+      Category.find(:all, :include => :posts).each do |category|
+        category.posts.collect(&:name)
+        category.entries.collect(&:name)
+      end
+      Bullet::Association.unpreload_associations.should_not be_empty
+      Bullet::Association.end_request
+    end
+
+    it "should detect no preload with category => [posts, entries]" do
+      Bullet::Association.start_request
+      Category.find(:all).each do |category|
+        category.posts.collect(&:name)
+        category.entries.collect(&:name)
       end
       Bullet::Association.unpreload_associations.should_not be_empty
       Bullet::Association.end_request
