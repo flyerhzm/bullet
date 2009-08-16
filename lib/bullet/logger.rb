f494f38f2b8f0de28b031d184d03c353e2f01478
Richard Huang <flyerhzm@gmail.com>
2009-08-16 13:22:07 +0800

    add introduction

--- lib/bullet/logger.rb
+++ lib/bullet/logger.rb
@@ -3,7 +3,7 @@ module Bullet
     LOG_FILE = ::RAILS_ROOT + '/log/bullet.log'
 
     def format_message(severity, timestamp, progname, msg)
-      "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+      "#{timestamp.to_formatted_s(:db)}[#{severity}] #{msg}\n"
     end
   end
 end
