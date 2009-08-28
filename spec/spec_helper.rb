048bc89e5d83c5a05befebbd58fb0801b5b0f509
Richard Huang <flyerhzm@gmail.com>
2009-08-28 22:36:44 +0800

    require dependency for spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'spec/autorun'
 require 'active_record'
+require 'action_controller'
 
 RAILS_ROOT = File.expand_path(__FILE__).split('/')[0..-3].join('/') unless defined? RAILS_ROOT
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/logger'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/active_record'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/association'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bulletware'))
 Bullet.enable = true
