281181161b30cde5d3c4fcebdda7ba89630e3065
Richard Huang <flyerhzm@gmail.com>
2009-09-11 09:47:43 +0800

    remove configuration.rb, just move configuration to bullet

--- lib/bullet.rb
+++ lib/bullet.rb
@@ -1,11 +1,11 @@
 require 'bulletware'
 
 module Bullet
-  @@enable = nil
-
   class <<self
+    attr_accessor :enable, :alert, :console, :growl, :growl_password, :rails_logger, :bullet_logger, :logger, :logger_file
+
     def enable=(enable)
-      @@enable = enable
+      @enable = enable
       if enable? 
         Bullet::ActiveRecord.enable
         ActionController::Dispatcher.middleware.use Bulletware
@@ -13,7 +13,27 @@ module Bullet
     end
 
     def enable?
-      @@enable == true
+      @enable == true
+    end
+
+    def growl=(growl)
+      if growl
+        begin
+          require 'ruby-growl'
+          growl = Growl.new('localhost', 'ruby-growl', ['Bullet Notification'], nil, @@growl_password)
+          growl.notify('Bullet Notification', 'Bullet Notification', 'Bullet Growl notifications have been turned on')
+        rescue MissingSourceFile
+          raise NotificationError.new('You must install the ruby-growl gem to use Growl notifications: `sudo gem install ruby-growl`')
+        end
+      end
+      @growl = growl
+    end
+
+    def bullet_logger=(bullet_logger)
+      if @bullet_logger = bullet_logger
+        @logger_file = File.open(Bullet::BulletLogger::LOG_FILE, 'a+')
+        @logger = Bullet::BulletLogger.new(@logger_file)
+      end
     end
   end
 
@@ -21,6 +41,5 @@ module Bullet
   autoload :Association, 'bullet/association'
   autoload :Counter, 'bullet/counter'
   autoload :BulletLogger, 'bullet/logger'
-  autoload :Configuration, 'bullet/configuration'
   autoload :Notification, 'bullet/notification'
 end
