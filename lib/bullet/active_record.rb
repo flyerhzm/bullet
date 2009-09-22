fce6c5f81801e8d419e6d24d786c97e88fa0ecda
Richard <richard@ekohe.com>
2009-09-22 18:56:09 +0800

    fix typo

--- lib/bullet/active_record.rb
+++ lib/bullet/active_record.rb
@@ -15,7 +15,7 @@ module Bullet
                 Bullet::Counter.add_possible_objects(records)
               elsif records.size == 1
                 Bullet::Association.add_impossible_object(records.first)
-                Bullet::Counter.add_impossible_object(records)
+                Bullet::Counter.add_impossible_object(records.first)
               end
             end
 
