2006261ec4b7bb1c61dc5c99f8e881b4441be390
Richard Huang <flyerhzm@gmail.com>
2009-10-13 23:52:31 +0800

    reload_application in ActionController::Dispacther in rails 2.2 is an instance method

--- lib/bullet/action_controller.rb
+++ lib/bullet/action_controller.rb
@@ -1,13 +1,24 @@
 module Bullet
   class ActionController
     def self.enable
-      ::ActionController::Dispatcher.class_eval do
-        class <<self
+      if Rails::VERSION::STRING.to_f < 2.3
+        ::ActionController::Dispatcher.class_eval do
           alias_method :origin_reload_application, :reload_application
           
           def reload_application
             origin_reload_application
             Bullet.clear
+          end
+        end
+      else
+        ::ActionController::Dispatcher.class_eval do
+          class <<self
+            alias_method :origin_reload_application, :reload_application
+            
+            def reload_application
+              origin_reload_application
+              Bullet.clear
+            end
           end
         end
       end
