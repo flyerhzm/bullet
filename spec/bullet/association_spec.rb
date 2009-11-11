68e2f4c4315b2c55344c0b8729201d33a8e9cc28
Richard Huang <flyerhzm@gmail.com>
2009-11-11 10:40:34 +0800

    restructure spec files

--- spec/bullet/association_spec.rb
+++ spec/bullet/association_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 
