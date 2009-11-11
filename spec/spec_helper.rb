54adab1396c1a88e70c3d213ead0845ff644ecff
Richard Huang <flyerhzm@gmail.com>
2009-11-11 10:31:53 +0800

    add lib to LOAD_PATH in test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,14 +10,10 @@ module Rails
 end
 
 RAILS_ROOT = File.expand_path(__FILE__).split('/')[0..-3].join('/') unless defined? RAILS_ROOT
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/notification'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/logger'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/active_record'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/action_controller'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/association'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet/counter'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bullet'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/bulletware'))
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
+require 'bullet'
+require 'bullet'
 Bullet.enable = true
 
 module BulletTestHelper
