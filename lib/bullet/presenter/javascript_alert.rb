29a12f7034372c2f222ba396c8ff654db364c041
Sven Riedel <sr@gimp.org>
2010-06-13 21:21:15 +0200

    Handle duplicate alerts by using a Set to store notices

--- lib/bullet/presenter/javascript_alert.rb
+++ lib/bullet/presenter/javascript_alert.rb
@@ -2,13 +2,11 @@ module Bullet
   module Presenter
     class JavascriptAlert < Base
       def self.active?
-        Rails.logger.debug "Active: #{Bullet.alert}"
         Bullet.alert
       end
 
       def self.inline( notice )
         return '' unless self.active?
-        Rails.logger.debug "Alert active!"
 
         JavascriptHelpers::wrap_js_association "alert( #{notice.full_notice.inspect} ); "
       end
