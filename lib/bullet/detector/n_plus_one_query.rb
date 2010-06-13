463bcef8c192dff8152b86389eed3b0495572cc3
Sven Riedel <sr@gimp.org>
2010-06-13 10:54:02 +0200

    Extract Detector::NPlusOneQuery

---
+++ lib/bullet/detector/n_plus_one_query.rb
@@ -0,0 +1,34 @@
+module Bullet
+  module Detector
+    class NPlusOneQuery < Association
+      def self.add_unpreload_associations(klazz, associations)
+        notice = Bullet::Notice::NPlusOneQuery.new( callers, klazz, associations )
+        Bullet.add_notification( notice )
+      end
+
+      # executed when object.assocations is called.
+      # first, it keeps this method call for object.association.
+      # then, it checks if this associations call is unpreload.
+      #   if it is, keeps this unpreload associations and caller.
+      def self.call_association(object, associations)
+        add_call_object_associations(object, associations)
+        if unpreload_associations?(object, associations)
+          add_unpreload_associations(object.class, associations)
+          caller_in_project
+        end
+      end
+
+      private
+      # decide whether the object.associations is unpreloaded or not.
+      def self.unpreload_associations?(object, associations)
+        possible?(object) and !impossible?(object) and !association?(object, associations)
+      end
+
+      def self.caller_in_project
+        vender_root ||= File.join(Rails.root, 'vendor')
+        callers << caller.select {|c| c =~ /#{Rails.root}/}.reject {|c| c =~ /#{vender_root}/}
+        callers.uniq!
+      end
+    end
+  end
+end
