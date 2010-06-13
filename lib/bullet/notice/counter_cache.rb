ec6c712c33e0e3241748f3591b3c76e08ac8f88e
Sven Riedel <sr@gimp.org>
2010-06-13 10:15:53 +0200

    Use new base initializer properly

--- lib/bullet/notice/counter_cache.rb
+++ lib/bullet/notice/counter_cache.rb
@@ -1,16 +1,6 @@
 module Bullet
   module Notice
     class CounterCache < Base
-      def initialize( base_class, associations, path = nil )
-        super( nil, nil, nil, nil )
-
-        @base_class = base_class
-        @associations = associations
-        @path = path
-
-        @response = counter_cache_messages
-      end
-
       def body
         klass_associations_str
       end
