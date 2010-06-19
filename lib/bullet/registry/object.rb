971529caa681383b1f12023bc81b50b37fa47ac5
Sven Riedel <sr@gimp.org>
2010-06-19 16:44:22 +0200

    Registry::Base

--- lib/bullet/registry/object.rb
+++ lib/bullet/registry/object.rb
@@ -1,28 +1,14 @@
 module Bullet
   module Registry
-    class Object
-      attr_reader :registry
-
-      def initialize
-        @registry = {}
-      end
-
+    class Object < Base
       def add( object_or_objects )
         klazz = object_or_objects.is_a?( Array ) ? object_or_objects.first.class :
                                                    object_or_objects.class
-        @registry[klazz] ||= []
-        @registry[klazz] << object_or_objects
-        unique( @registry[klazz] )
+        super( klazz, object_or_objects )
       end
 
       def contains?( object )
         @registry[object.class] and @registry[object.class].include?( object )
-      end
-
-      private
-      def unique( array )
-        array.flatten!
-        array.uniq!
       end
     end
   end
