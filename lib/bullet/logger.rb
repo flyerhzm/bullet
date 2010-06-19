bf16b64b10f4827be2ef61009489c133b0230199
Sven Riedel <sr@gimp.org>
2010-06-19 17:10:46 +0200

    Remove now unused bullet/logger.rb

--- lib/bullet/logger.rb
+++
@@ -1,9 +0,0 @@
-module Bullet
-  class BulletLogger < Logger
-    LOG_FILE = File.join(Rails.root, 'log/bullet.log')
-
-    def format_message(severity, timestamp, progname, msg)
-      "#{timestamp.to_formatted_s(:db)}[#{severity}] #{msg}\n"
-    end
-  end
-end
