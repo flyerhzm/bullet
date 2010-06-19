5991bbe9cae9c3e1c24872a7e6ef4c41b735b5af
Sven Riedel <sr@gimp.org>
2010-06-19 16:53:08 +0200

    Use Registry for object_associations and call_object_associations

--- lib/bullet/registry/association.rb
+++ lib/bullet/registry/association.rb
@@ -11,22 +11,6 @@ module Bullet
           key.include?( base ) and value == associations
         end.collect( &:first ).flatten!
       end
-
-      def [](base)
-        @registry[base]
-      end
-
-      def delete( base )
-        @registry.delete( base )
-      end
-
-      def select( *args, &block )
-        @registry.select( *args, &block )
-      end
-
-      def each( &block ) 
-        @registry.each( &block )
-      end
     end
   end
 end
