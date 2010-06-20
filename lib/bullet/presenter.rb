ec6408494fef763e702e775e7100083629e20564
Sven Riedel <sr@gimp.org>
2010-06-20 09:34:47 +0200

    Add initial xmpp presenter

--- lib/bullet/presenter.rb
+++ lib/bullet/presenter.rb
@@ -4,6 +4,7 @@ module Bullet
     autoload :JavascriptAlert, 'bullet/presenter/javascript_alert'
     autoload :JavascriptConsole, 'bullet/presenter/javascript_console'
     autoload :Growl, 'bullet/presenter/growl'
+    autoload :Xmpp, 'bullet/presenter/xmpp'
     autoload :BulletLogger, 'bullet/presenter/bullet_logger'
     autoload :RailsLogger, 'bullet/presenter/rails_logger'
   end
