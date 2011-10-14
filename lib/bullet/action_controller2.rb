ef8956fa97875ebc5f3b39af101b0c711f116206
Richard Huang <flyerhzm@gmail.com>
2011-10-14 09:54:08 +0800

    fix typo

--- lib/bullet/action_controller2.rb
+++ lib/bullet/action_controller2.rb
@@ -3,7 +3,7 @@ module Bullet
     def self.enable
       require 'action_controller'
       case Rails.version
-      when /^2.3/  
+      when /^2.3/
         ::ActionController::Dispatcher.middleware.use Bullet::Rack
         ::ActionController::Dispatcher.class_eval do
           class <<self
@@ -22,20 +22,20 @@ module Bullet
             Bullet.clear
           end
         end
-        
+
         ::ActionController::Base.class_eval do
           alias_method :origin_process, :process
           def process(request, response, method = :perform_action, *arguments)
             Bullet.start_request
             response = origin_process(request, response, method = :perform_action, *arguments)
-            
+
             if Bullet.notification?
               if response.headers["type"] and response.headers["type"].include? 'text/html' and response.body =~ %r{<html.*</html>}m
                 response.body <<= Bullet.gather_inline_notifications
                 response.headers["Content-Length"] = response.body.length.to_s
               end
-            
-              Bullet.perform_bullet_out_of_channel_notifications
+
+              Bullet.perform_out_of_channel_notifications
             end
             Bullet.end_request
             response
