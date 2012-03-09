4ba67bf60a15cb70c658192d9dcad96d7b93cd20
Richard Huang <flyerhzm@gmail.com>
2012-03-08 17:34:27 -0800

    abstract table schema, model definitions, test db seed from association_for_chris_spec to spec_helper

---
+++ spec/models/location.rb
@@ -0,0 +1,3 @@
+class Location < ActiveRecord::Base
+  has_many :hotels
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -145,6 +145,20 @@ def setup_db
       t.column :name, :string
       t.column :category_id, :integer
     end
+
+    create_table :locations do |t|
+      t.column :name, :string
+    end
+
+    create_table :hotels do |t|
+      t.column :name, :string
+      t.column :location_id, :integer
+    end
+
+    create_table :deals do |t|
+      t.column :name, :string
+      t.column :hotel_id, :integer
+    end
   end
 end
 
@@ -254,6 +268,19 @@ RSpec.configure do |config|
     submission2 = category1.submissions.create(:name => "submission2", :user => user2)
     submission3 = category2.submissions.create(:name => "submission3", :user => user1)
     submission4 = category2.submissions.create(:name => "submission4", :user => user2)
+
+    location1 = Location.create(:name => "location1")
+    location2 = Location.create(:name => "location2")
+
+    hotel1 = location1.hotels.create(:name => "hotel1")
+    hotel2 = location1.hotels.create(:name => "hotel2")
+    hotel3 = location2.hotels.create(:name => "hotel3")
+    hotel4 = location2.hotels.create(:name => "hotel4")
+
+    deal1 = hotel1.deals.create(:name => "deal1")
+    deal2 = hotel2.deals.create(:name => "deal2")
+    deal3 = hotel3.deals.create(:name => "deal3")
+    deal4 = hotel4.deals.create(:name => "deal4")
   end
 
   config.after(:all) do
