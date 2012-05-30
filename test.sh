fe995a4d6c807cc8b6a9587e5fb3e7f78a4572c7
Richard Huang <flyerhzm@gmail.com>
2012-05-30 08:38:40 +0800

    add test.sh to test all stuff

---
+++ test.sh
@@ -0,0 +1,4 @@
+bundle && bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.1.4 bundle && bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.0.12 bundle && bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle && bundle exec rspec spec
