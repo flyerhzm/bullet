a0d0dbb9f18c848860f21d5292e3e6ef000e5e21
Richard Huang <flyerhzm@gmail.com>
2012-06-06 21:02:32 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.1.4 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.0.12 bundle && bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.1.5 bundle && bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.0.13 bundle && bundle exec rspec spec
 BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle && bundle exec rspec spec
