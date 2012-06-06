94ccd28a7a464f3e4bfcb211b4f81ec40e4232ab
Richard Huang <flyerhzm@gmail.com>
2012-06-06 20:15:57 +0800

    autoload spec models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,7 @@ extend Bullet::Dependency
 Bullet.enable = true
 
 MODELS = File.join(File.dirname(__FILE__), "models")
+$LOAD_PATH.unshift(MODELS)
 SUPPORT = File.join(File.dirname(__FILE__), "support")
 Dir[ File.join(SUPPORT, "*.rb") ].reject { |filename| filename =~ /_seed.rb$/ }.sort.each { |file| require file }
 
@@ -35,7 +36,10 @@ if active_record?
   ActiveRecord::Migration.verbose = false
 
   # Autoload every active_record model for the test suite that sits in spec/models.
-  Dir[ File.join(MODELS, "*.rb") ].sort.each { |file| require file }
+  Dir[ File.join(MODELS, "*.rb") ].sort.each do |filename|
+    name = File.basename(filename, ".rb")
+    autoload name.camelize.to_sym, name
+  end
   require File.join(SUPPORT, "sqlite_seed.rb")
 
   RSpec.configure do |config|
