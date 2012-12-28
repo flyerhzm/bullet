5128dafaeadb371951a0061fd761cc1a48aeffaf
Richard Huang <flyerhzm@gmail.com>
2012-12-28 22:33:39 +0800

    run test.sh with correct Gemfile*.lock files

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.1.5 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.0.13 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-4-beta bundle && bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.1.5 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.1.5 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.0.13 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.0.13 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle && BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-4-beta bundle && BUNDLE_GEMFILE=Gemfile.rails-4-beta bundle exec rspec spec
