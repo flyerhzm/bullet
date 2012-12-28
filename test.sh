63827d95744bb7a803746ed46d499160d8658945
Richard Huang <flyerhzm@gmail.com>
2012-12-28 23:22:11 +0800

    support mongoid 2.5.x

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 bundle && bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.1.5 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.1.5 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.0.13 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.0.13 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.2.9 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.2.9 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.1.8 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.1.8 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.0.17 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.0.17 bundle exec rspec spec
 BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle && BUNDLE_GEMFILE=Gemfile.rails-2.3.14 bundle exec rspec spec
 BUNDLE_GEMFILE=Gemfile.rails-4-beta bundle && BUNDLE_GEMFILE=Gemfile.rails-4-beta bundle exec rspec spec
