ccd405b2a054b30008ea56babdf322a497921f48
Richard Huang <flyerhzm@gmail.com>
2014-02-16 16:55:22 +0800

    rename all Gemfiles

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #bundle update rails && bundle exec rspec spec
 #BUNDLE_GEMFILE=Gemfile.mongoid bundle update mongoid && BUNDLE_GEMFILE=Gemfile.mongoid bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-4.1.0 bundle && BUNDLE_GEMFILE=Gemfile.rails-4.1.0 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-4.0.1 bundle && BUNDLE_GEMFILE=Gemfile.rails-4.0.1 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.2.15 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.2.15 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.1.12 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.1.12 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-3.0.20 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.0.20 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-3.1.6 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-3.1.6 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-3.0.23 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-3.0.23 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-2.8.1 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.8.1 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-2.7.1 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.7.1 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-2.6.0 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.6.0 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-2.5.2 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.5.2 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.mongoid-2.4.12 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.4.12 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-4.1 bundle && BUNDLE_GEMFILE=Gemfile.rails-4.1 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-4.0 bundle && BUNDLE_GEMFILE=Gemfile.rails-4.0 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.2 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.2 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.1 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.1 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-3.0 bundle && BUNDLE_GEMFILE=Gemfile.rails-3.0 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-3.1 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-3.1 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-3.0 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-3.0 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-2.8 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.8 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-2.7 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.7 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-2.6 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.6 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-2.5 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.5 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.mongoid-2.4 bundle && BUNDLE_GEMFILE=Gemfile.mongoid-2.4 bundle exec rspec spec
