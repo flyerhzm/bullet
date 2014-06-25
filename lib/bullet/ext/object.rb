548ae99542efc4694c712001bdb1201f87fe70a6
Richard Huang <flyerhzm@gmail.com>
2014-06-25 16:49:28 +0800

    primay_key can't be nil

--- lib/bullet/ext/object.rb
+++ lib/bullet/ext/object.rb
@@ -1,6 +1,6 @@
 class Object
   def bullet_ar_key
-    if self.class.respond_to? :primary_key
+    if self.class.respond_to?(:primary_key) && self.class.primary_key
       "#{self.class}:#{self.send self.class.primary_key}"
     else
       "#{self.class}:#{self.id}"
