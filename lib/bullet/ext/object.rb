025f5fe95508d5b1c17448fc64efc69c288531da
Richard Huang <flyerhzm@gmail.com>
2014-06-25 16:19:56 +0800

    use respond_to? instead of is_a? to check primary_key

--- lib/bullet/ext/object.rb
+++ lib/bullet/ext/object.rb
@@ -1,6 +1,6 @@
 class Object
   def bullet_ar_key
-    if self.is_a? ActiveRecord::Base
+    if self.class.respond_to? :primary_key
       "#{self.class}:#{self.send self.class.primary_key}"
     else
       "#{self.class}:#{self.id}"
