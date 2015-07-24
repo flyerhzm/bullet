52b373974a91e3c4ae53d20c5b536d9c5956fa68
Joel Low <joel@joelsplace.sg>
2015-07-24 18:26:50 +0800

    Do not report association queries immediately after object creation to require a preload.

--- lib/bullet/active_record42.rb
+++ lib/bullet/active_record42.rb
@@ -21,6 +21,16 @@ module Bullet
         end
       end
 
+      ::ActiveRecord::Persistence.class_eval do
+        def save_with_bullet(*args, &proc)
+          was_new_record = new_record?
+          save_without_bullet(*args, &proc).tap do |result|
+            Bullet::Detector::NPlusOneQuery.add_impossible_object(self) if result && was_new_record
+          end
+        end
+        alias_method_chain :save, :bullet
+      end
+
       ::ActiveRecord::Relation.class_eval do
         alias_method :origin_to_a, :to_a
         # if select a collection of objects, then these objects have possible to cause N+1 query.
