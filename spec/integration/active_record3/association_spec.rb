986192cb8bdf945464243563b791023b0997b62c
Daniel Low <danglow@gmail.com>
2016-02-09 20:28:35 -0800

    Fix for #243 in ActiveRecord 3.0/3.x
    This applies the fix in #243 and #244 for rails 3.0/3.x

--- spec/integration/active_record3/association_spec.rb
+++ spec/integration/active_record3/association_spec.rb
@@ -537,6 +537,27 @@ if !mongoid? && active_record3?
     end
   end
 
+  describe Bullet::Detector::Association, "query immediately after creation" do
+    context "document => children" do
+      it 'should not detect non preload associations' do
+        document1 = Document.new
+        document1.children.build
+        document1.save
+
+        document2 = Document.new(parent: document1)
+        document2.save
+        document2.parent
+
+        document1.children.each.first
+
+        Bullet::Detector::UnusedEagerLoading.check_unused_preload_associations
+        expect(Bullet::Detector::Association).not_to be_has_unused_preload_associations
+
+        expect(Bullet::Detector::Association).to be_completely_preloading_associations
+      end
+    end
+  end
+
   describe Bullet::Detector::Association, "STI" do
     context "page => author" do
       it "should detect non preload associations" do
