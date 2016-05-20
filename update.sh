eda9b088f122bd201f0c64f7f7d02a8076c306ef
Surim Kim <kimsuelim@gmail.com>
2016-05-20 11:50:11 +0900

    support mongoid 6.0.0

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ BUNDLE_GEMFILE=Gemfile.rails-4.0 bundle update
 BUNDLE_GEMFILE=Gemfile.rails-3.2 bundle update
 BUNDLE_GEMFILE=Gemfile.rails-3.1 bundle update
 BUNDLE_GEMFILE=Gemfile.rails-3.0 bundle update
+BUNDLE_GEMFILE=Gemfile.mongoid-6.0 bundle update
 BUNDLE_GEMFILE=Gemfile.mongoid-5.0 bundle update
 BUNDLE_GEMFILE=Gemfile.mongoid-4.0 bundle update
 BUNDLE_GEMFILE=Gemfile.mongoid-3.1 bundle update
