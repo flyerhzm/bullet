f28e3de1f04851b3cd1604173fe631e367799264
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2016-12-19 19:41:47 +0100

    avoid double backtrace

--- spec/bullet/notification/unused_eager_loading_spec.rb
+++ spec/bullet/notification/unused_eager_loading_spec.rb
@@ -6,7 +6,7 @@ module Bullet
       subject { UnusedEagerLoading.new([""], Post, [:comments, :votes], "path") }
 
       it { expect(subject.body).to eq("  Post => [:comments, :votes]\n  Remove from your finder: :includes => [:comments, :votes]") }
-      it { expect(subject.title).to eq("Unused Eager Loading in path") }
+      it { expect(subject.title).to eq("AVOID eager loading in path") }
     end
   end
 end
