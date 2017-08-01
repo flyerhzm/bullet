1f7e64eab6fe2380ad81a99be252742d5bec7463
Awesome Code <team@xinminlabs.com>
2017-08-01 00:38:52 +0000

    Auto corrected by following Space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ Bullet.enable = true
 MODELS = File.join(File.dirname(__FILE__), 'models')
 $LOAD_PATH.unshift(MODELS)
 SUPPORT = File.join(File.dirname(__FILE__), 'support')
-Dir[ File.join(SUPPORT, '*.rb') ].reject { |filename| filename =~ /_seed.rb$/ }.sort.each { |file| require file }
+Dir[File.join(SUPPORT, '*.rb')].reject { |filename| filename =~ /_seed.rb$/ }.sort.each { |file| require file }
 
 RSpec.configure do |config|
   config.extend Bullet::Dependency
@@ -41,7 +41,7 @@ if active_record?
   ActiveRecord::Migration.verbose = false
 
   # Autoload every active_record model for the test suite that sits in spec/models.
-  Dir[ File.join(MODELS, '*.rb') ].sort.each do |filename|
+  Dir[File.join(MODELS, '*.rb')].sort.each do |filename|
     name = File.basename(filename, '.rb')
     autoload name.camelize.to_sym, name
   end
@@ -71,7 +71,7 @@ end
 
 if mongoid?
   # Autoload every mongoid model for the test suite that sits in spec/models.
-  Dir[ File.join(MODELS, 'mongoid', '*.rb') ].sort.each { |file| require file }
+  Dir[File.join(MODELS, 'mongoid', '*.rb')].sort.each { |file| require file }
   require File.join(SUPPORT, 'mongo_seed.rb')
 
   RSpec.configure do |config|
