384e155855ed18646a9df84216f3b655e213481a
sshaw <skye.shaw@gmail.com>
2019-10-10 22:36:00 -0400

    Use modern includes syntax in warnings

--- lib/bullet/notification/n_plus_one_query.rb
+++ lib/bullet/notification/n_plus_one_query.rb
@@ -10,7 +10,7 @@ module Bullet
       end
 
       def body
-        "#{klazz_associations_str}\n  Add to your finder: #{associations_str}"
+        "#{klazz_associations_str}\n  Add to your query: #{associations_str}"
       end
 
       def title
