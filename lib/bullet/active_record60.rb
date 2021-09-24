4b4c3fcd13ba4fae872f113d0c97140b0caaa85d
Awesome Code <team@xinminlabs.com>
2021-09-24 07:35:58 +0000

    Auto corrected by following Lint Ruby Style/SoleNestedConditional

--- lib/bullet/active_record60.rb
+++ lib/bullet/active_record60.rb
@@ -44,8 +44,7 @@ module Bullet
           # if select only one object, then the only one object has impossible to cause N+1 query.
           def records
             result = super
-            if Bullet.start?
-              if result.first.class.name !~ /^HABTM_/
+            if Bullet.start? && (result.first.class.name !~ /^HABTM_/)
                 if result.size > 1
                   Bullet::Detector::NPlusOneQuery.add_possible_objects(result)
                   Bullet::Detector::CounterCache.add_possible_objects(result)
@@ -54,7 +53,6 @@ module Bullet
                   Bullet::Detector::CounterCache.add_impossible_object(result.first)
                 end
               end
-            end
             result
           end
         end
@@ -138,8 +136,7 @@ module Bullet
           end
 
           def construct(ar_parent, parent, row, seen, model_cache)
-            if Bullet.start?
-              unless ar_parent.nil?
+            if Bullet.start? && !ar_parent.nil?
                 parent.children.each do |node|
                   key = aliases.column_alias(node, node.primary_key)
                   id = row[key]
@@ -153,7 +150,6 @@ module Bullet
                   @bullet_eager_loadings[ar_parent.class][ar_parent] << associations
                 end
               end
-            end
 
             super
           end
@@ -227,8 +223,7 @@ module Bullet
           def target
             result = super()
 
-            if Bullet.start?
-              if owner.class.name !~ /^HABTM_/ && !@inversed
+            if Bullet.start? && (owner.class.name !~ /^HABTM_/ && !@inversed)
                 if is_a? ::ActiveRecord::Associations::ThroughAssociation
                   Bullet::Detector::NPlusOneQuery.call_association(owner, reflection.through_reflection.name)
                   association = owner.association(reflection.through_reflection.name)
@@ -248,7 +243,6 @@ module Bullet
                   Bullet::Detector::NPlusOneQuery.add_possible_objects(result) if result
                 end
               end
-            end
             result
           end
         end
