b4dd7b4541a205b4e48d3700cf55f8197bef3509
Camilo Vargas A <vargas.antiguay@gmail.com>
2022-08-25 13:31:38 -0400

    Remove useless freeze

--- lib/bullet/detector/n_plus_one_query.rb
+++ lib/bullet/detector/n_plus_one_query.rb
@@ -38,17 +38,17 @@ module Bullet
           primary_key_values_are_empty = true
           keys_joined = ""
           objects.each do |obj|
-            unless obj.class.name =~ /^HABTM_/.freeze
+            unless obj.class.name =~ /^HABTM_/
               class_names_match_regex = false
             end
             unless obj.bullet_primary_key_value.nil?
               primary_key_values_are_empty = false
             end
-            keys_joined += "#{(keys_joined.empty?? ''.freeze : ', '.freeze)}#{obj.bullet_key}"
+            keys_joined += "#{(keys_joined.empty?? '' : ', ')}#{obj.bullet_key}"
           end
           unless class_names_match_regex || primary_key_values_are_empty
             Bullet.debug(
-              'Detector::NPlusOneQuery#add_possible_objects'.freeze,
+              'Detector::NPlusOneQuery#add_possible_objects',
               "objects: #{keys_joined}"
             )
             objects.each { |object| possible_objects.add object.bullet_key }
