f84a88c02f60e254dcefc8150f443132b4700fc9
Ildar Kayumov <kaayumov@gmail.com>
2024-08-10 10:04:45 +0200

    fix: store global objects into thread-local variables

--- lib/bullet.rb
+++ lib/bullet.rb
@@ -147,43 +147,36 @@ module Bullet
       puts "[Bullet][#{title}] #{message}" if ENV['BULLET_DEBUG'] == 'true'
     end
 
+    def thread_local_data
+      Thread.current.thread_variable_get(:bullet) || Thread.current.thread_variable_set(:bullet, {})
+    end
+
     def start_request
-      Thread.current[:bullet_start] = true
-      Thread.current[:bullet_notification_collector] = Bullet::NotificationCollector.new
+      thread_local_data[:bullet_start] = true
+      thread_local_data[:bullet_notification_collector] = Bullet::NotificationCollector.new
 
-      Thread.current[:bullet_object_associations] = Bullet::Registry::Base.new
-      Thread.current[:bullet_call_object_associations] = Bullet::Registry::Base.new
-      Thread.current[:bullet_possible_objects] = Bullet::Registry::Object.new
-      Thread.current[:bullet_impossible_objects] = Bullet::Registry::Object.new
-      Thread.current[:bullet_inversed_objects] = Bullet::Registry::Base.new
-      Thread.current[:bullet_eager_loadings] = Bullet::Registry::Association.new
-      Thread.current[:bullet_call_stacks] = Bullet::Registry::CallStack.new
+      thread_local_data[:bullet_object_associations] = Bullet::Registry::Base.new
+      thread_local_data[:bullet_call_object_associations] = Bullet::Registry::Base.new
+      thread_local_data[:bullet_possible_objects] = Bullet::Registry::Object.new
+      thread_local_data[:bullet_impossible_objects] = Bullet::Registry::Object.new
+      thread_local_data[:bullet_inversed_objects] = Bullet::Registry::Base.new
+      thread_local_data[:bullet_eager_loadings] = Bullet::Registry::Association.new
+      thread_local_data[:bullet_call_stacks] = Bullet::Registry::CallStack.new
 
-      Thread.current[:bullet_counter_possible_objects] ||= Bullet::Registry::Object.new
-      Thread.current[:bullet_counter_impossible_objects] ||= Bullet::Registry::Object.new
+      thread_local_data[:bullet_counter_possible_objects] ||= Bullet::Registry::Object.new
+      thread_local_data[:bullet_counter_impossible_objects] ||= Bullet::Registry::Object.new
     end
 
     def end_request
-      Thread.current[:bullet_start] = nil
-      Thread.current[:bullet_notification_collector] = nil
-
-      Thread.current[:bullet_object_associations] = nil
-      Thread.current[:bullet_call_object_associations] = nil
-      Thread.current[:bullet_possible_objects] = nil
-      Thread.current[:bullet_impossible_objects] = nil
-      Thread.current[:bullet_inversed_objects] = nil
-      Thread.current[:bullet_eager_loadings] = nil
-
-      Thread.current[:bullet_counter_possible_objects] = nil
-      Thread.current[:bullet_counter_impossible_objects] = nil
+      Thread.current.thread_variable_set(:bullet, nil)
     end
 
     def start?
-      enable? && Thread.current[:bullet_start]
+      enable? && thread_local_data[:bullet_start]
     end
 
     def notification_collector
-      Thread.current[:bullet_notification_collector]
+      thread_local_data[:bullet_notification_collector]
     end
 
     def notification?
