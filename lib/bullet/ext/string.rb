b10ed992aa4d5dc26ca6494e84de1741541a68b5
Vincent Prigent <vincent.prigent1@gmail.com>
2025-01-16 14:40:53 +1300

    Use memoization on refined class Avoid recreating strings within n_plus_one_query

--- lib/bullet/ext/string.rb
+++ lib/bullet/ext/string.rb
@@ -4,8 +4,13 @@ module Bullet
   module Ext
     module String
       refine ::String do
+        attr_reader :bullet_class_name
+
         def bullet_class_name
-          sub(/:[^:]*?$/, '')
+          @bullet_class_name ||= begin
+            last_colon = self.rindex(':')
+            last_colon ? self[0...last_colon] : self
+          end
         end
       end
     end
